4ad952ed5cc5a35798dc026cafdc22b433f3954a
b-prathap <prathap.bly@gmail.com>
2016-11-13 22:09:04 +0530

    Branch Android 2

--- app/src/main/java/com/example/prathapb/myapplication/MainActivity.java
+++ app/src/main/java/com/example/prathapb/myapplication/MainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+//afdgafdgadfgadf
+
 
 public class MainActivity extends AppCompatActivity {
 
--- app/src/main/java/com/example/prathapb/myapplication/TestMainActivity.java
+++ app/src/main/java/com/example/prathapb/myapplication/TestMainActivity.java
@@ -8,6 +8,8 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+//sdgasdgasgasg
+
 
 public class MainActivity extends AppCompatActivity {
 
